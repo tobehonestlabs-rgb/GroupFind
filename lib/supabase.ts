93d4fdfa1fcb9cf5305a55e601cfe66c6d570274
calvin <calvinunsafe@gmail.com>
2026-07-31 02:52:58 +0000

    fixed supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -7,7 +7,7 @@ const SUPABASE_ANON_KEY =
   process.env.NEXT_SUPABASE_ANON_PUBLIC_KEY ||
   '';
 const SUPABASE_SERVICE_ROLE =
-  process.env.NEXT_SUPABASE_SERVICE_ROLE_KEY ||
+  process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY ||
   process.env.SUPABASE_SERVICE_ROLE_KEY ||
   '';
 const GROUPS_TABLE = process.env.NEXT_PUBLIC_SUPABASE_GROUPS || 'groupes';
