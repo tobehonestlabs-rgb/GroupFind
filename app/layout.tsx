6a755968431d707ef761db4fe5d070ab603f80f9
calvin <calvinunsafe@gmail.com>
2026-08-01 17:49:14 +0000

    error fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 // app/layout.tsx
 import type { Metadata } from 'next';
 import './globals.css';
-import Header from '../components/AuthHeader.tsx';
+import Header from '../components/AuthHeader';
 
 export const metadata: Metadata = {
   title: 'GroupFind.ci',
